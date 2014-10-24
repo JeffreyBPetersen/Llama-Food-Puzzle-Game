ee796788ae6f0aeff6f127acbfc3269842bb2522
Jeffrey Petersen <jeffreybpetersen@gmail.com>
2014-10-24 12:37:58 -0700

    Add board code and tweaks
    Set code to run once page finishes loading; made global variable for
    board size; added code for board state including pieces; added debug
    function to randomize board.

--- puzzle_dots.js
+++ puzzle_dots.js
@@ -1,4 +1,64 @@
-drawGrid(3);
+// Global default for board size
+var board_size = 3
+
+// Executed when page is loaded
+function launch(){
+	drawGrid(board_size)
+	board = new Board(board_size)
+}
+
+function DEBUG_randomize_board(density){ // density is a number from 0 to 1
+	for(var i = 0; i < board.size; i++){
+		for(var j = 0; j < board.size; j++){
+			if(Math.random() < density){
+				var color, direction;
+				switch(Math.floor(Math.random() * 3)){
+					case 0:
+						color = "#FF0000"
+						break
+					case 1:
+						color = "#00FF00"
+						break;
+					case 2:
+						color = "#0000FF"
+						break;
+				}
+				switch(Math.floor(Math.random() * 4)){
+					case 0:
+						direction = "up"
+						break;
+					case 1:
+						direction = "right"
+						break;
+					case 2:
+						direction = "down"
+						break;
+					case 3:
+						direction = "left"
+						break;
+				}
+				board.space[i][j] = new Dot(color, direction)
+			}
+		}
+	}
+}
+
+// Board constructor
+function Board(size){ // size is any positive integer
+	this.size = size
+	
+	//Initialize 2D array of spaces
+	this.space = new Array()
+	for(var i = 0; i < size; i++){
+		this.space[i] = new Array()
+	}
+}
+
+// Takes color code and direction, ex: board.space[1][2] = new Dot("#0000FF", "up")
+function Dot(color, direction){ // color is a hex code, direction is "up", "right", "down", or "left"
+	this.color = color
+	this.direction = direction
+}
 
 function drawGrid(size)
 {
