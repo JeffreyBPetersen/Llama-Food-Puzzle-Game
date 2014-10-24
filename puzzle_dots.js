a5576a696c9ef1f9dbdd9e3c0912bf21baf98b21
Connor Dunham <cjdunham@ucsc.edu>
2014-10-24 13:33:10 -0700

    drawGrid now takes board as argument
    Adds x_padding, x_spacing, and y_spacing as properties of board.

--- puzzle_dots.js
+++ puzzle_dots.js
@@ -1,10 +1,19 @@
 // Global default for board size
-var board_size = 3
+var board_size = 3;
 
 // Executed when page is loaded
 function launch(){
-	drawGrid(board_size)
 	board = new Board(board_size)
+        drawGrid(board);
+        
+        DEBUG_randomize_board(.3);
+        for (i = 0; i < board_size; i++)
+        {
+            for (j = 0; j < board_size; j++)
+            {
+                console.log("(" + i + ", " + j + ")    " + board.space[i][j]);
+            }
+        }
 }
 
 function DEBUG_randomize_board(density){ // density is a number from 0 to 1
@@ -46,6 +55,9 @@ function DEBUG_randomize_board(density){ // density is a number from 0 to 1
 // Board constructor
 function Board(size){ // size is any positive integer
 	this.size = size
+        this.x_padding = document.getElementById('game_canvas').width / 5;   //padding on sides of board 
+        this.x_spacing = (document.getElementById('game_canvas').width - 2 * this.x_padding) / size;   //width of each rectangle on board
+        this.y_spacing = document.getElementById('game_canvas').height / size;   //height of each rectangle on board
 	
 	//Initialize 2D array of spaces
 	this.space = new Array()
@@ -60,39 +72,37 @@ function Dot(color, direction){ // color is a hex code, direction is "up", "righ
 	this.direction = direction
 }
 
-function drawGrid(size)
+// Takes a board object and draws a grid for the board (not including dots)
+function drawGrid(board)
 {
     var canvas = document.getElementById('game_canvas');
     var ctx = canvas.getContext('2d');
-    var x_padding = canvas.width / 5;   //padding on sides of board for buttons
-    var x_spacing = (canvas.width - 2 * x_padding) / size;  //width of each rectangle on board
-    var y_spacing = canvas.height / size;   //height of each rectangle on board
     
     //draws lines separating board from sides
     ctx.beginPath();
-    ctx.moveTo(x_padding, 0);
-    ctx.lineTo(x_padding, canvas.height);
+    ctx.moveTo(board.x_padding, 0);
+    ctx.lineTo(board.x_padding, canvas.height);
     ctx.stroke();
-    ctx.moveTo(canvas.width - x_padding, 0);
-    ctx.lineTo(canvas.width - x_padding, canvas.height);
+    ctx.moveTo(canvas.width - board.x_padding, 0);
+    ctx.lineTo(canvas.width - board.x_padding, canvas.height);
     ctx.stroke();
     
     //draws inner board lines
-    for (i = 1; i < size; i++)
+    for (i = 1; i < board.size; i++)
     {
-        ctx.moveTo(i * x_spacing + x_padding, 0);
-        ctx.lineTo(i * x_spacing + x_padding, canvas.height);
+        ctx.moveTo(i * board.x_spacing + board.x_padding, 0);
+        ctx.lineTo(i * board.x_spacing + board.x_padding, canvas.height);
         ctx.stroke();
     }
-    for (i = 1; i < size; i++)
+    for (i = 1; i < board.size; i++)
     {
-        ctx.moveTo(x_padding, i * y_spacing);
-        ctx.lineTo(canvas.width - x_padding, i * y_spacing);
+        ctx.moveTo(board.x_padding, i * board.y_spacing);
+        ctx.lineTo(canvas.width - board.x_padding, i * board.y_spacing);
         ctx.stroke();
     }
     
     //colors the spaces to the left and right of board
     ctx.fillStyle = "#9932CC";
-    ctx.fillRect(0, 0, x_padding, canvas.height)
-    ctx.fillRect(canvas.width - x_padding, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, board.x_padding, canvas.height)
+    ctx.fillRect(canvas.width - board.x_padding, 0, canvas.width, canvas.height);
 }
